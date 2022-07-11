bd8e116337e9e142fe15ea88907da0b5d87533c2
Rodrigo <rodrigopsalazar@hotmail.com>
2022-07-11 22:13:21 +0200

    remove sass

--- transposition-frontend/src/components/note-selector/index.tsx
+++ transposition-frontend/src/components/note-selector/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { NOTES } from '../../utils/notes';
 import Button from '../button';
-import './NoteSelector.scss';
+import './../../styles/output.css';
 
 function NoteSelector({ setSelected, selected, selectedNotation }) {
     return (
-        <div className="note-selector">
+        <div className="grid grid-cols-4">
             {NOTES.map((note, k) => (
                 <Button
                     key={k}
                     onClick={() => setSelected(k)}
                     disabled={k === selected}
+                    className={note.english.includes('/') ? 'bg-black' : ''}
                 >
                     {note[`${selectedNotation}`]}
                 </Button>
