3d7c71ad8e23d497fe50c91c5b852c784c21fe84
Rodrigo <rodrigopsalazar@hotmail.com>
2022-07-12 17:15:03 +0200

    add german notation

--- transposition-frontend/src/utils/notes.ts
+++ transposition-frontend/src/utils/notes.ts
@@ -1,21 +1,22 @@
 export const NOTES: Note[] = [
-    { romance: 'do', english: 'C' },
-    { romance: 'do♯ / ré♭', english: 'C♯ / D♭' },
-    { romance: 'ré', english: 'D' },
-    { romance: 'ré♯ / si♭', english: 'D♯ / E♭' },
-    { romance: 'mi', english: 'E' },
-    { romance: 'fa', english: 'F' },
-    { romance: 'fa♯ / sol♭', english: 'F♯ / G♭' },
-    { romance: 'sol', english: 'G' },
-    { romance: 'sol♯ / la♭', english: 'G♯ / A♭' },
-    { romance: 'la', english: 'A' },
-    { romance: 'la♯ / si♭', english: 'A♯ / B♭' },
-    { romance: 'si', english: 'B' },
+    { romance: 'do', english: 'C', german: 'c' },
+    { romance: 'do♯ / ré♭', english: 'C♯ / D♭', german: 'cis / des' },
+    { romance: 'ré', english: 'D', german: 'd' },
+    { romance: 'ré♯ / si♭', english: 'D♯ / E♭', german: 'dis / es' },
+    { romance: 'mi', english: 'E', german: 'e' },
+    { romance: 'fa', english: 'F', german: 'f' },
+    { romance: 'fa♯ / sol♭', english: 'F♯ / G♭', german: 'fis / ges' },
+    { romance: 'sol', english: 'G', german: 'g' },
+    { romance: 'sol♯ / la♭', english: 'G♯ / A♭', german: 'gis / as' },
+    { romance: 'la', english: 'A', german: 'a' },
+    { romance: 'la♯ / si♭', english: 'A♯ / B♭', german: 'ais / b' },
+    { romance: 'si', english: 'B', german: 'h' },
 ];
 
 export interface Note {
     romance: string;
     english: string;
+    german: string;
 }
 
 export function getNote(index: number, notation: keyof Note) {
