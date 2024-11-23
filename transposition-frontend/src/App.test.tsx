984fdf129f9eadbadab9cda44d2357cbae3b846c
Rodrigo <rodrigopsalazar@hotmail.com>
2024-11-23 22:52:39 +0100

    add transposition core functions unit tests

--- transposition-frontend/src/App.test.tsx
+++ transposition-frontend/src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+    it('renders without crashing', () => {
+        render(<App />);
+        // Replace 'Welcome' with some text or element you expect to see in the App component
+        const linkElement = screen.getByText(/welcome/i);
+        expect(linkElement).toBeInTheDocument();
+    });
 });
