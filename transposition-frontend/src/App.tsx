11b4d31cdbb0014bc66611142b4ae7f1b22ee80b
Rodrigo <rodrigopsalazar@hotmail.com>
2024-12-07 18:02:00 +0100

    extract contexts from app

--- transposition-frontend/src/App.tsx
+++ transposition-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import './styles/output.css';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
@@ -7,162 +7,67 @@ import CrossInstrumentsScaleTransposition from './pages/scale-transposition/cros
 import AboutPage from './pages/about';
 import LandingPage from './pages/landing';
 import { BottomNav, Footer, Header } from './header';
-import { Note } from './utils/notes';
-import { useIsMobile } from './hooks/useIsMobile';
-import { Language } from './hooks/useTranslation';
 import IntervalsScaleTransposition from './pages/scale-transposition/intervals';
-import LanguageContext from './contexts/LanguageContext';
-import NotationContext from './contexts/NotationContext';
-
-const detectUserBrowserLanguage = (): Language => {
-    const userLanguage = navigator.language.toLowerCase().split('-')[0];
-    const localStorageLanguage = localStorage.getItem(
-        'selectedLanguage'
-    ) as Language;
-
-    if (localStorageLanguage) {
-        return localStorageLanguage;
-    }
-
-    switch (userLanguage) {
-        case 'fr':
-            return Language.French;
-        // Add more cases for other supported languages
-        default:
-            return Language.English;
-    }
-};
-
-const defaultNotation = (): keyof Note => {
-    const userLanguage = navigator.language.toLowerCase().split('-')[0];
-
-    const localStorageNotation = localStorage.getItem('selectedNotation') as
-        | keyof Note
-        | null;
-
-    if (localStorageNotation) {
-        return localStorageNotation;
-    }
-
-    switch (userLanguage) {
-        case 'fr':
-            return 'romance';
-        case 'es':
-            return 'romance';
-        case 'it':
-            return 'romance';
-        case 'pt':
-            return 'romance';
-        case 'de':
-            return 'german';
-        case 'nl':
-            return 'german';
-        default:
-            return 'english';
-    }
-};
+import ContextsProvider from './contexts/ContextsProvider';
 
 function App() {
-    const [selectedNotation, setSelectedNotation]: [keyof Note, any] =
-        useState(defaultNotation);
-    const [selectedLanguage, setSelectedLanguage] = useState(
-        detectUserBrowserLanguage
-    );
-    const isMobile = useIsMobile();
-
-    function handleChangeNotation(notation: keyof Note) {
-        setSelectedNotation(notation);
-        localStorage.setItem('selectedNotation', selectedNotation);
-    }
-
-    function handleChangeLanguage(language: Language) {
-        setSelectedLanguage(language);
-        localStorage.setItem('selectedLanguage', language);
-    }
-
-    useEffect(() => {
-        localStorage.setItem('selectedNotation', selectedNotation);
-    }, [selectedNotation]);
-
-    useEffect(() => {
-        localStorage.setItem('selectedLanguage', selectedLanguage);
-    }, [selectedLanguage]);
-
     return (
         <div className="App container mx-auto overflow-clip">
             <BrowserRouter>
-                <LanguageContext.Provider
-                    value={{
-                        selectedLanguage,
-                        setSelectedLanguage: handleChangeLanguage,
-                    }}
-                >
-                    <NotationContext.Provider
-                        value={{
-                            selectedNotation,
-                            setSelectedNotation: handleChangeNotation,
-                        }}
-                    >
-                        <Header />
-                        <main className="w-full m-auto flex flex-col items-center">
-                            <div className={`contents flex p-2 z-0 relative`}>
-                                <Routes>
-                                    <Route path="/" element={<LandingPage />} />
-                                    <Route path="note">
-                                        <Route
-                                            index
-                                            element={
-                                                <Navigate to="0-0-0" replace />
-                                            }
-                                        />
-                                        <Route
-                                            path=":linkParams"
-                                            element={<SimpleTransposition />}
-                                        />
-                                    </Route>
-                                    <Route path="scale-cross-instruments">
-                                        <Route
-                                            index
-                                            element={
-                                                <Navigate
-                                                    to="0-0-0-0"
-                                                    replace
-                                                />
-                                            }
-                                        />
-                                        <Route
-                                            path=":linkParams"
-                                            element={
-                                                <CrossInstrumentsScaleTransposition />
-                                            }
-                                        />
-                                    </Route>
-                                    <Route path="scale-intervals">
-                                        <Route
-                                            index
-                                            element={
-                                                <Navigate to="0-5-up" replace />
-                                            }
-                                        />
-                                        <Route
-                                            path=":linkParams"
-                                            element={
-                                                <IntervalsScaleTransposition />
-                                            }
-                                        />
-                                    </Route>
+                <ContextsProvider>
+                    <Header />
+                    <main className="w-full m-auto flex flex-col items-center">
+                        <div className={`contents flex p-2 z-0 relative`}>
+                            <Routes>
+                                <Route path="/" element={<LandingPage />} />
+                                <Route path="note">
                                     <Route
-                                        path="about"
-                                        element={<AboutPage />}
+                                        index
+                                        element={
+                                            <Navigate to="0-0-0" replace />
+                                        }
                                     />
-                                    <Route path="*" element={<LandingPage />} />
-                                </Routes>
-                            </div>
-                        </main>
-                        <BottomNav />
-                        <Footer />
-                    </NotationContext.Provider>
-                </LanguageContext.Provider>
+                                    <Route
+                                        path=":linkParams"
+                                        element={<SimpleTransposition />}
+                                    />
+                                </Route>
+                                <Route path="scale-cross-instruments">
+                                    <Route
+                                        index
+                                        element={
+                                            <Navigate to="0-0-0-0" replace />
+                                        }
+                                    />
+                                    <Route
+                                        path=":linkParams"
+                                        element={
+                                            <CrossInstrumentsScaleTransposition />
+                                        }
+                                    />
+                                </Route>
+                                <Route path="scale-intervals">
+                                    <Route
+                                        index
+                                        element={
+                                            <Navigate to="0-5-up" replace />
+                                        }
+                                    />
+                                    <Route
+                                        path=":linkParams"
+                                        element={
+                                            <IntervalsScaleTransposition />
+                                        }
+                                    />
+                                </Route>
+                                <Route path="about" element={<AboutPage />} />
+                                <Route path="*" element={<LandingPage />} />
+                            </Routes>
+                        </div>
+                    </main>
+                    <BottomNav />
+                    <Footer />
+                </ContextsProvider>
             </BrowserRouter>
         </div>
     );
---
+++ transposition-frontend/src/contexts/ContextsProvider.tsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react';
+import { Note } from '../utils/notes';
+import { Language } from '../hooks/useTranslation';
+import LanguageContext from '../contexts/LanguageContext';
+import NotationContext from '../contexts/NotationContext';
+
+const detectUserBrowserLanguage = (): Language => {
+    const userLanguage = navigator.language.toLowerCase().split('-')[0];
+    const localStorageLanguage = localStorage.getItem(
+        'selectedLanguage'
+    ) as Language;
+
+    if (localStorageLanguage) {
+        return localStorageLanguage;
+    }
+
+    switch (userLanguage) {
+        case 'fr':
+            return Language.French;
+        case 'es':
+            return Language.Spanish;
+        case 'de':
+            return Language.German;
+        default:
+            return Language.English;
+    }
+};
+
+const defaultNotation = (): keyof Note => {
+    const userLanguage = navigator.language.toLowerCase().split('-')[0];
+
+    const localStorageNotation = localStorage.getItem('selectedNotation') as
+        | keyof Note
+        | null;
+
+    if (localStorageNotation) {
+        return localStorageNotation;
+    }
+
+    switch (userLanguage) {
+        case 'fr':
+            return 'romance';
+        case 'es':
+            return 'romance';
+        case 'it':
+            return 'romance';
+        case 'pt':
+            return 'romance';
+        case 'de':
+            return 'german';
+        case 'nl':
+            return 'german';
+        default:
+            return 'english';
+    }
+};
+
+function ContextsProvider({ children }: { children: React.ReactNode }) {
+    const [selectedNotation, setSelectedNotation]: [keyof Note, any] =
+        useState(defaultNotation);
+    const [selectedLanguage, setSelectedLanguage] = useState(
+        detectUserBrowserLanguage
+    );
+
+    function handleChangeNotation(notation: keyof Note) {
+        setSelectedNotation(notation);
+        localStorage.setItem('selectedNotation', notation.toString());
+    }
+
+    function handleChangeLanguage(language: Language) {
+        setSelectedLanguage(language);
+        localStorage.setItem('selectedLanguage', language);
+    }
+
+    useEffect(() => {
+        localStorage.setItem('selectedNotation', selectedNotation.toString());
+    }, [selectedNotation]);
+
+    useEffect(() => {
+        localStorage.setItem('selectedLanguage', selectedLanguage);
+    }, [selectedLanguage]);
+
+    return (
+        <>
+            <LanguageContext.Provider
+                value={{
+                    selectedLanguage,
+                    setSelectedLanguage: handleChangeLanguage,
+                }}
+            >
+                <NotationContext.Provider
+                    value={{
+                        selectedNotation,
+                        setSelectedNotation: handleChangeNotation,
+                    }}
+                >
+                    {children}
+                </NotationContext.Provider>
+            </LanguageContext.Provider>
+        </>
+    );
+}
+
+export default ContextsProvider;
