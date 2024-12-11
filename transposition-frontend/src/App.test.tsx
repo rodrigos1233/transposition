4667bfa7146cb3496b79773704cfb45545708a8f
Rodrigo <rodrigopsalazar@hotmail.com>
2024-12-11 22:31:01 +0100

    support old scale links

--- transposition-frontend/src/App.test.tsx
+++ transposition-frontend/src/App.test.tsx
@@ -5,7 +5,6 @@ import App from './App';
 describe('App Component', () => {
     it('renders without crashing', () => {
         render(<App />);
-        // Replace 'Welcome' with some text or element you expect to see in the App component
         const linkElement = screen.getByText(/welcome/i);
         expect(linkElement).toBeInTheDocument();
     });
