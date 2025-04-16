c29b4a2de59dfacf9d22a14bf2e66efe46c8d9fc
Rodrigo <rodrigopsalazar@hotmail.com>
2025-04-16 15:17:26 +0200

    replace CRA with Vite

---
+++ transposition-frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 3000, // Same as CRA default
+  },
+  build: {
+    outDir: 'build', // Same as CRA default
+  },
+});
